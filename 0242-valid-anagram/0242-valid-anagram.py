062d8c8f9ff7835ae22758622b5dd7436252d841
Vinod Gawde <47134554+vinodgawde@users.noreply.github.com>
2024-12-04 00:00:03 +0530

    Time: 11 ms (75.55%), Space: 17.3 MB (29.97%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s_hash={}
+        t_hash={}
+        for char in s:
+            if char in s_hash:
+                s_hash[char]+=1
+            else:
+                s_hash[char]=1
+        for char in t:
+            if char in t_hash:
+                t_hash[char]+=1
+            else:
+                t_hash[char]=1  
+        if s_hash == t_hash:
+            return True
+        return False  
